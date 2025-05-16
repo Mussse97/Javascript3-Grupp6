adabc08e1422c37fea78656648bf25b8fa68f364
Tobias-Thor <tobiasthor@protonmail.com>
2025-05-16 08:51:11 +0200

    Fixat kommenteringen, OBSERVERA ATT DENNA BRANCH HAR ÄNDRINGAR I App.css SAMT index.css FÖR ATT MÖRKT/LJUST TEMA OCH BAKGRUNDSFÄRG PÅ SIDAN SKULLE FUNGERA KORREKT

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+// Importerar nödvändiga bibliotek och komponenter 
 import React, { useEffect, useState } from 'react';
 import Footer from './components/footer/Footer';
 import { Header } from './components/Header';
 import './App.css';
 import Explore from './components/Explore';
 
+// Definierar huvudkomponenten för appen som hanterar mörkt/ljust läge och renderar andra komponenter
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
   const toggleTheme = () => setIsDarkMode(prev => !prev);
-
+  // useState-hooken hanterar tillståndet för mörkt/ljust läge, toggleTheme-funktionen växlar mellan lägena
   useEffect(() => {
     document.body.className = isDarkMode ? 'dark-theme' : 'light-theme';
   }, [isDarkMode]);
-
+  // useEffect-hooken används, uppdaterar klassnamnet på body-elementet på det aktuella läget (mörkt eller ljust)
+  
+  // Renderar komponenter, Header-komponenten tar emot toggleTheme-funktionen som prop
   return (
     <>
       <Header toggleTheme={toggleTheme} />
@@ -21,4 +25,5 @@ function App() {
   );
 }
 
+// Exporterar App-komponenten för användning i andra delar av appen 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
+// Importerar nödvändiga bibliotek och komponenter 
 import React from 'react';
 import './Header.css';
 import Navbar from './Navbar';
 
+// Definierar Header-komponenten som representerar sidhuvudet i applikationen
+// Header-komponenten innehåller en logotyp, ett navigeringsfält och en hamburgermeny
 const Header = ({ toggleTheme }) => {
   return (
     <header className="header">
@@ -11,22 +14,8 @@ const Header = ({ toggleTheme }) => {
   );
 };
 
+// Exporterar Header-komponenten för användning i andra delar av appen
+// Header-komponenten tar emot toggleTheme-funktionen som prop för att växla mellan mörkt/ljust läge
 export default Header;
 
 
-/*Importerar React och Navbar-komponenten  
-import React from 'react';
-import './Header.css';
-import Navbar from './Navbar';
-
-// Definierar Header-komponenten som representerar sidhuvudet i applikationen
-// Header-komponenten innehåller en logotyp, ett navigeringsfält och en hamburgermeny
-
-export const Header = ({ toggleTheme }) => {
-  return (
-    <header className="header">
-      <h1 className="header-logo">MedieTema</h1>
-      <Navbar toggleTheme={toggleTheme} />
-    </header>
-  );
-};*/
