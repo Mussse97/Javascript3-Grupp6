726ba0266114634f79c0db2ef9da1a742d15ee64
Jonas E <jonas_eklof89@hotmail.com>
2025-05-16 09:23:51 +0200

    tog bort död kod

--- grupp6-sanity/schemaTypes/index.js
+++ grupp6-sanity/schemaTypes/index.js
@@ -1,9 +1,5 @@
-import books from './books'
-import movies from './movies'
-import games from './games'
-import music from './music'
 import category from './category'
 import genre from './genre'
 import post from './post'
 
-export const schemaTypes = [books, movies, games, music, category, genre, post]
+export const schemaTypes = [category, genre, post]
