caff0dd86151be418393670669751c144d68c15d
Tobias-Thor <tobiasthor@protonmail.com>
2025-05-17 13:53:52 +0200

    fixat med registrering av användarkonto och justering av CSS för textarea i registreringsformuläret

--- grupp6-sanity/schemaTypes/index.js
+++ grupp6-sanity/schemaTypes/index.js
@@ -2,7 +2,8 @@
 import category from "./category"
 import genre from "./genre"
 import post from "./post"
+import user from "./user"
 
 
 
-export const schemaTypes = [category, genre, post]
+export const schemaTypes = [category, genre, post, user]
