a76b1a4775aeb4176541d913368ac41b98b96573
Mussse97 <89797827+Mussse97@users.noreply.github.com>
2025-05-21 10:28:45 +0200

    StartsidaFilter + createPost fungerar!

--- grupp6-sanity/schemaTypes/post.js
+++ grupp6-sanity/schemaTypes/post.js
@@ -1,3 +1,98 @@
+// export default {
+//   name: 'post',
+//   type: 'document',
+//   title: 'Inlägg',
+//   fields: [
+//     {
+//       name: 'title',
+//       type: 'string',
+//       title: 'Titel',
+//     },
+//     {
+//       name: 'slug',
+//       type: 'slug',
+//       title: 'Slug',
+//       options: {
+//         source: 'title',
+//         maxLength: 96,
+//         auto: true,
+//       },
+//     },
+//     {
+//       name: 'year',
+//       type: 'number',
+//       title: 'År',
+//     },
+//     {
+//       name: 'producer',
+//       type: 'string',
+//       title: 'Producent',
+//     },
+//     {
+//       name: 'category',
+//       type: 'reference',
+//       title: 'Kategori',
+
+//       to: [{type: 'category'}],
+//     },
+//     {
+//       name: 'genres',
+//       type: 'array',
+//       title: 'Genrer',
+
+//       of: [{type: 'reference', to: [{type: 'genre'}]}],
+//     },
+//     {
+//       name: 'body',
+//       type: 'text',
+//       title: 'Innehåll',
+//     },
+
+//       {
+//     name: 'likes',
+//     type: 'number',
+//     title: 'Likes',
+//     initialValue: 0,
+//   },
+//   {
+//     name: 'dislikes',
+//     type: 'number',
+//     title: 'Dislikes',
+//     initialValue: 0,
+
+//   },
+//     {
+
+//       name: 'comments',
+//       type: 'array',
+//       title: 'Kommentarer',
+//       of: [
+//         {
+//           type: 'object',
+//           fields: [
+//             {
+//               name: 'name',
+//               type: 'string',
+//               title: 'Namn',
+//             },
+//             {
+//               name: 'comment',
+//               type: 'text',
+//               title: 'Kommentar',
+//             },
+//             {
+//               name: 'createdAt',
+//               type: 'datetime',
+//               title: 'Skapad',
+//               initialValue: () => new Date(c.createdAt).toLocaleDateString('sv-SE'),
+//             },
+//           ],
+//         },
+//       ],
+//     },
+//   ],
+// }
+
 export default {
   name: 'post',
   type: 'document',
@@ -20,7 +115,7 @@ export default {
     },
     {
       name: 'year',
-      type: 'number',
+      type: 'number',  
       title: 'År',
     },
     {
@@ -30,39 +125,40 @@ export default {
     },
     {
       name: 'category',
-      type: 'reference',
+      type: 'reference',  
       title: 'Kategori',
-
-      to: [{type: 'category'}],
+      to: [{ type: 'category' }],
     },
     {
       name: 'genres',
-      type: 'array',
+      type: 'array',  
       title: 'Genrer',
-
-      of: [{type: 'reference', to: [{type: 'genre'}]}],
+      of: [{ type: 'reference', to: [{ type: 'genre' }] }],
     },
     {
-      name: 'body',
+      name: 'body',  
       type: 'text',
-      title: 'Innehåll',
+      title: 'Recension/Innehåll',
     },
-
-      {
-    name: 'likes',
-    type: 'number',
-    title: 'Likes',
-    initialValue: 0,
-  },
-  {
-    name: 'dislikes',
-    type: 'number',
-    title: 'Dislikes',
-    initialValue: 0,
-
-  },
     {
-
+      name: 'createdAt',
+      type: 'datetime',
+      title: 'Publicerad',
+      initialValue: () => new Date().toISOString(),  
+    },
+    {
+      name: 'likes',
+      type: 'number',
+      title: 'Likes',
+      initialValue: 0,
+    },
+    {
+      name: 'dislikes',
+      type: 'number',
+      title: 'Dislikes',
+      initialValue: 0,
+    },
+    {
       name: 'comments',
       type: 'array',
       title: 'Kommentarer',
@@ -84,11 +180,11 @@ export default {
               name: 'createdAt',
               type: 'datetime',
               title: 'Skapad',
-              initialValue: () => new Date(c.createdAt).toLocaleDateString('sv-SE'),
+              initialValue: () => new Date().toISOString(),  
             },
           ],
         },
       ],
     },
   ],
-}
+};
